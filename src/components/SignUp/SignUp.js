b17fb83fbd6c505c2ac86905e25bd7ce4c014e34
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-04-11 20:26:30 +0600

    Sign Up section useState and onBlur,onSubmit use

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
 const SignUp = () => {
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const [confirmPassword,setConfirmPassword]=useState('');
+    const [error,setError]=useState('');
+
+    const handleEmailBlur=event=>{
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur=event=>{
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur=event=>{
+        setConfirmPassword(event.target.value)
+    }
+    const handleCreateUser=event=>{
+      event.preventDefault();
+      if(password !== confirmPassword ){
+          setError("Your two passwords didn't match");
+          return;
+      }
+    }
+
     return (
         <div className="form-container">
         <div>
         <h1 className='form-title'>Sign Up</h1>
-         <form>
+         <form onSubmit={handleCreateUser}>
          <div className="input-group">
              <label htmlFor="email">Email</label>
-             <input type="email" name="email" id="" required />
+             <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
          </div>
          <div className="input-group">
              <label htmlFor="password">Password</label>
-             <input type="password" name="password" id="" required />
+             <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
          </div>
          <div className="input-group">
              <label htmlFor="confirm-password">Confirm-Password</label>
-             <input type="password" name="password" id="" required />
+             <input onBlur={handleConfirmPasswordBlur} type="password" name="password" id="" required />
          </div>
-         <button className='form-submit' type="submit">Sign Up</button>
+         <p style={{color:'red'}}>
+             {error}
+         </p>
+         <button className='form-submit' type="Sign Up">Sign Up</button>
          </form>
          <p className='createAccountLinkParent'>
              Already Have An Account?<Link className='form-link' to="/Login">Login</Link>
